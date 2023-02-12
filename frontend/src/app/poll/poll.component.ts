f0bfe858070ed936ffb729d0a7a01608ee390bf1
Aman <aman.verma@docquity.com>
2023-02-13 00:13:21 +0530

    poll-response added

--- frontend/src/app/poll/poll.component.ts
+++ frontend/src/app/poll/poll.component.ts
@@ -1,21 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { DataService } from 'src/service/data-service';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
-  styleUrls: ['./poll.component.css']
+  styleUrls: ['./poll.component.css'],
 })
 export class PollComponent {
+  message: any;
+  pollData: any;
+  optionRes: any;
 
-  message:any
-  pollData : any
+  options = {"optA" : 1, "optB" : 2, "optC" : 3, "optD" : 4,}
 
-  constructor(private data:DataService){}
+  constructor(private data: DataService, private http: HttpClient) {}
 
-  ngOnInit(){
-    this.data.currentMessage.subscribe(message => this.message=message)
-    this.pollData = this.message
+  ngOnInit() {
+    this.data.currentMessage.subscribe((message) => (this.message = message));
+    this.pollData = this.message;
   }
 
+  userResponse(optID: number) {
+    let userID = uuid();
+    let userAnswer = { pollID: this.message.pollID, optionID: optID};
+
+    this.http.post('http://localhost:3000/option-counter', userAnswer).subscribe(
+      (res) => {
+        this.optionRes = res
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+
+  }
 }
